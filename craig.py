17a3f62ed41a0cae8f21da19d6f458226e128f8b
ben norquist <norquist.ben@gmail.com>
2017-07-31 22:40:06 -0700

    started init for base

--- craig.py
+++ craig.py
@@ -1,4 +1,6 @@
 import logging
+from bs4 import BeautifulSoup
+import requests
 
 fh = logging.FileHandler(filename='craigs.log')
 logging.basicConfig(level=logging.INFO, handlers=fh)
@@ -11,17 +13,59 @@ def record_meta(spider):
 class CraigslistBase(object):
     """ Base class for all Craiglist wrappers. Retrieve all metadata for given filters"""
 
-    def __init__(self):
-        self.url
-        self.id
-        self.title
+    def __init__(self, site=None, area=None, category=None, filters=None):
+
+        #self.url
+        #self.id
+        #self.title
+        self.sites_url = 'http://www.craigslist.org/about/sites'
+        self.all_sites = self.get_all_sites()
         self.meta = record_meta()
+        self.category = category
+
+        if self.site not in self.all_sites:
+            msg = "{} is not a valid site".format(self.site)
+            logger.error(msg)
+            raise ValueError(msg)
+        self.site = site
+
+        if area:
+            if not self.is_valid_area(area):
+                msg = "'%s' is not a valid area for site '%s'" % (area, site)
+                self.logger.error(msg)
+                raise ValueError(msg)
+        self.area = area
+
+
+    def is_valid_area(self, area):
+        """Check if area is valid"""
+        base_url = self.url_templates['base']
+        response = requests.get(base_url.format({'site': self.site}))
+        soup = BeautifulSoup(response.content, 'html.parser')
+        sublinks = soup.find('ul', {'class': 'sublinks'})
+        return sublinks and sublinks.find('a', text=area) is not None
+
+    def get_all_sites(self):
+        response = requests.get(self.sites_url)
+        response.raise_for_status()  # Something failed?
+        soup = BeautifulSoup(response.content, 'html.parser')
+        sites = set()
+
+        for box in soup.findAll('div', {'class': 'box'}):
+            for a in box.findAll('a'):
+                # Remove protocol and get subdomain
+                site = a.attrs['href'].rsplit('//', 1)[1].split('.')[0]
+                sites.add(site)
+
+        return sites
 
     url_templates = {
         'base': 'http://{site}.craigslist.org',
         'no_area': 'http://{site}.craigslist.org/search/{category}',
         'area': 'http://{site}.craigslist.org/search/{area}/{category}'
     }
+
+
 
     default_site = 'sfbay'
 
